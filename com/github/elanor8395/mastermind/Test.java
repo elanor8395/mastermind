40acd2e24471b79157dce243e8ab9295db4e2117
dima <mingazov@email.com>
2019-07-03 18:02:49 +0200

    testing

---
+++ com/github/elanor8395/mastermind/Test.java
@@ -0,0 +1,8 @@
+package com.github.elanor8395.mastermind;
+
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("I'm mastermind and i'm working");
+    }
+}
